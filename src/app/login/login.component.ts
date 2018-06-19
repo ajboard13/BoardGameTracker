933babc5bb4ac09c7b563f675f81a34bc7bea414
Aaron <ajboard13@gmail.com>
2018-06-19 09:44:17 -0400

    login styling done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,23 +3,29 @@ import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
 import { Router } from '@angular/router';
 import { moveIn } from '../router.animations';
 import * as firebase from 'firebase/app';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  animations: [moveIn()],
-  host: {'[@moveIn]': ''}
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  cardForm: FormGroup;
 
   error: any;
-  constructor(public af: AngularFireAuth,private router: Router) {
+  constructor(public af: AngularFireAuth,private router: Router, private fb: FormBuilder) {
 
       this.af.authState.subscribe(auth => { 
       if(auth) {
         this.router.navigateByUrl('/members');
       }
+    });
+    this.cardForm = fb.group({
+      materialFormCardNameEx: ['', Validators.required],
+      materialFormCardEmailEx: ['', [Validators.email, Validators.required]],
+      materialFormCardConfirmEx: ['', Validators.required],
+      materialFormCardPasswordEx: ['', Validators.required]
     });
 
   }
